483475720fb06d6262f786dcf77dfc238d2565ef
Eduardo Rodríguez <eduardorc190299@gmail.com>
2024-06-27 18:50:14 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EstateAgents.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
